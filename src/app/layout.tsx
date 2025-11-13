2636647e603d7a2c848b14b59952d7ee0851ab56
damola <thedamolaoladipo@gmail.com>
2025-11-13 11:28:07 +0100

    chore: modified root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,44 +3,43 @@ import "./globals.css";
 import Header from "@/components/Layout/Header";
 import Footer from "@/components/Layout/Footer";
 import { ThemeProvider } from "next-themes";
-import ScrollToTop from '@/components/ScrollToTop';
+import ScrollToTop from "@/components/ScrollToTop";
 import Aoscompo from "@/utils/aos";
 import { DonationProvider } from "./context/donationContext";
 import SessionProviderComp from "@/components/nextauth/SessionProvider";
 import { AuthDialogProvider } from "./context/AuthDialogContext";
-const montserrat = Montserrat({ subsets: ["latin"] });
-import NextTopLoader from 'nextjs-toploader';
+import NextTopLoader from "nextjs-toploader";
+import { getServerSession } from "next-auth";
+import { GET } from "@/app/api/auth/[...nextauth]/route";
 
-export default function RootLayout({
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export default async function RootLayout({
   children,
-  session,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-  session:any
-}>) {
+}) {
+  // Fetch session on the server
+  const session = await getServerSession(GET);
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={montserrat.className}>
-      <NextTopLoader color="#FF4D7E" />
-      <DonationProvider>
-        <AuthDialogProvider>
-      <SessionProviderComp session={session}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={true}
-          defaultTheme="system"
-        >
-          <Aoscompo>
-            <Header />
-            
-            {children}
-            
-            <Footer />
-          </Aoscompo>
-          <ScrollToTop />
-        </ThemeProvider>
-        </SessionProviderComp>
-        </AuthDialogProvider>
+        <NextTopLoader color="#FF4D7E" />
+        <DonationProvider>
+          <AuthDialogProvider>
+            <SessionProviderComp session={session}>
+              <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+                <Aoscompo>
+                  <Header />
+                  {children}
+                  <Footer />
+                </Aoscompo>
+                <ScrollToTop />
+              </ThemeProvider>
+            </SessionProviderComp>
+          </AuthDialogProvider>
         </DonationProvider>
       </body>
     </html>
