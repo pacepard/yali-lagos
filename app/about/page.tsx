f7b769c6c4377bde150e7d7dec4419f481f60517
brodrickfavour <brodrickfavour0@gmail.comm>
2025-12-16 09:52:37 -0800

    updated about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,10 +8,10 @@ export default function AboutPage() {
       <div className="pt-20 pb-10 container flex min-h-[50vh] flex-col items-center justify-center text-center">
         {/* Hero Content */}
         <div className="text-center">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 sm:mb-6 text-gray-900">
+          <h1 className="text-6xl sm:text-7xl font-bold mb-6 text-gray-900">
             About us
           </h1>
-          <p className="text-base sm:text-lg md:text-xl text-gray-700 max-w-2xl mx-auto">
+          <p className="text-xl text-gray-700">
             Welcome to YALI Network Lagos. Where impact is our benchmark.
           </p>
         </div>
